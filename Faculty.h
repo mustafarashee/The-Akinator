8c9183511fc265db047b8a206bdd903a7224fff9
mustafarashee <mustafarashseed23@yahoo.com>
2018-06-20 21:38:52 +0500

    first commit

---
+++ Faculty.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Gender.h"
+using namespace std;
+
+class faculty:public dt
+{
+public:
+void Faculty(){
+a=teacher.faculty();
+
+if(a==1)
+{
+gen.permenent();
+}
+else{
+gen.visiting();
+}
+}
+protected:
+int a;
+
+dt teacher;
+teacher_gender gen;
+};
---
+++ Gender.h
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#include "associate_prof.h"
+using namespace std;
+
+class teacher_gender:public dt
+{
+public:
+void permenent()
+{
+a=teach.gender();
+if (a==1)
+{
+teacher.m_associate();
+}
+else
+{
+  cout<<"per female";
+}
+}
+void visiting()
+{
+a=teach.gender();
+{
+  if (a==1)
+{
+cout<<"visiting male";
+}
+else
+{
+  cout<<"visiting female";
+}
+
+}
+}
+  protected:
+  dt teach;
+  associate teacher;
+  int a;
+  
+};
